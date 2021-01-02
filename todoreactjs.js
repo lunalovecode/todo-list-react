27c70a7d00191db5b972ef16138290e6403c41a0
lunalovecode <65585638+lunalovecode@users.noreply.github.com>
2021-01-02 12:00:57 +0800

    Upload files

---
+++ todoreactjs.js
@@ -0,0 +1,11 @@
+/*
+TODO:
+	Textbox and new TODO
+	Add checkbox to newly created TODO
+	Automatically add to item count and unchecked count
+	Decrease unchecked count (but not item count) when checkbox is checked
+	Increase unchecked count if checkbox is unchecked
+	On double click, edit text in TODO
+	Press enter while editing to confirm that you're done editing.
+*/
+
